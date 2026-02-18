79e47c2182da44ce4e5277969c60e1464a22f0db
djfnd <daniel.fowler06@gmail.com>
2026-02-18 10:54:31 +0100

    Fix base path

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/publishing-data-capture/',
+  base: '/user-led-publishing-data-capture/',
 })
